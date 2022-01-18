fe3c381eb64d5deb42e57fbd518e90e6bbcd2a9d
Juan Chauta <jmchauta@gmail.com>
2022-01-17 23:31:57 -0500

    Add git aliases

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,21 +7,29 @@ echo
 echo
 
 # Install Oh My Zsh
+echo
 echo "--- Installing OhMyZsh"
+echo
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install powerlevel10k
+echo
 echo "--- Installing powerlevel10k"
+echo
 sh -c "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
 # Install tig
 if ! command -v tig &> /dev/null; then
+  echo
   echo "--- Installing tig"
+  echo
   sudo apt-get install -y tig
 fi
 
 # Config files
+echo
 echo "--- Linking dotfiles"
+echo
 
 ln -sf ~/dotfiles/configs/.aliases ~/.aliases
 echo "--- ~/dotfiles/configs/.aliases -> ~/.aliases"
@@ -45,3 +53,19 @@ echo "--- ~/dotfiles/.zlogin -> ~/.zlogin"
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 echo "--- ~/dotfiles/.zshrc -> ~/.zshrc"
 
+# Git Aliases
+echo
+echo "--- Setting Up Git Global Aliases"
+echo
+git config --global alias.co checkout
+echo "--- Setting Git alias - [checkout]: co"
+git config --global alias.br branch
+echo "--- Setting Git alias - [branch]: br"
+git config --global alias.ci commit
+echo "--- Setting Git alias - [commit]: ci"
+git config --global alias.st status
+echo "--- Setting Git alias - [status]: st"
+git config --global alias.unstage 'reset HEAD --'
+echo "--- Setting Git alias - [reset HEAD --]: unstage"
+git config --global alias.last git 'log -p -1'
+echo "--- Setting Git alias - [log -p -1]: last"
